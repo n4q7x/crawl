b4ea2ca93479251229b6e1200944c2c735eebace
n4q7x <t0lz@tutamail.com>
2025-11-24 11:14:43 -0700

    Updated crawl.py

---
+++ crawl.py
@@ -0,0 +1,20 @@
+
+
+import sys
+
+'''
+
+The sys module is how you can interact with certain objects
+which the Python interpreter itself has access to, 
+particularly things like command-line arguments,
+stdin, stdout, and stderr, modules,
+and information about the runtime environment, 
+like the operating system and Python version.
+It can also be used for debugging.
+
+In this program, we are just using it
+to grab any command-line arguments the user might pass.
+But in the beginning, we may not even need that.
+
+'''
+
